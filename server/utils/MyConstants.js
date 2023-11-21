7419772ff374f741f07c460a85d0fc61cbe4b079
phuongle2kar3 <150886869+phuongle2kar3@users.noreply.github.com>
2023-11-21 14:55:01 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.fwltptx.mongodb.net',
+    DB_USER: 'hoaiphuongle23112003',
+    DB_PASS: 'Abc123',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'Le Hoai Phuong',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'hoaiphuongle23.11.2003@gmail.com', // gmail service
+    EMAIL_PASS: 'qsybqldvvmvljhtm'
+  };
+  module.exports = MyConstants;
